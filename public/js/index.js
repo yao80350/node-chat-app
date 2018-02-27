02736f0401a8813b666a6b7c9d5beb42ca134dda
Yao <151736726@qq.com>
2018-02-27 21:27:40 +0800

    add createMessage and newMessage events

---
+++ public/js/index.js
@@ -0,0 +1,19 @@
+var socket = io();
+
+// 前端网页es6兼容少，所以改es5
+socket.on('connect', function() {
+    console.log('Connected to server');
+
+    socket.emit('createMessage', {
+        to: 'yao',
+        text: 'Hey. This is Cindy.'
+    });
+});
+
+socket.on('disconnect', function() {
+    console.log('Disconnected from server');
+});
+
+socket.on('newMessage', function(message) {
+    console.log('newMessage', message);
+});
--- server/server.js
+++ server/server.js
@@ -1,13 +1,35 @@
 const path = require('path'); //nodejs 内置module
-const express = require('express');
+const http = require('http'); //nodejs
+const express = require('express'); //利用nodejs的http请求
+const socketIO = require('socket.io'); // websocket 请求 后端的库
 
 const publicPath = path.join(__dirname, '../public');
 const port = process.env.PORT || 3000;
 
 let app = express();
+let server = http.createServer(app); // 默认使用http请求，这里是为了区分出来
+let io = socketIO(server);
 
 app.use(express.static(publicPath));
 
-app.listen(port, () => {
+io.on('connection', (socket) => { // websocket事件 --- 连接
+    console.log('New user connected');
+
+    socket.emit('newMessage', {
+        form: "cindy",
+        text: "Hey, What is going on.",
+        createAt: 123
+    });
+
+    socket.on('createMessage', (message) => {
+        console.log('createMessage', message);
+    });
+
+    socket.on('disconnect', () => {
+        console.log('User was disconnected');
+    });
+});
+
+server.listen(port, () => {
     console.log(`Server is up on ${port}`);
 });
