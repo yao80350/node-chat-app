30fff2313a911700e77b0463105474fcbfbbb418
Yao <151736726@qq.com>
2018-02-28 22:05:36 +0800

    create generateMessage

--- server/server.js
+++ server/server.js
@@ -3,6 +3,8 @@ const http = require('http'); //nodejs
 const express = require('express'); //利用nodejs的http请求
 const socketIO = require('socket.io'); // websocket 请求 后端的库
 
+const {generateMessage} = require('./utils/message');
+
 const publicPath = path.join(__dirname, '../public');
 const port = process.env.PORT || 3000;
 
@@ -15,24 +17,13 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => { // websocket事件 --- 连接
     console.log('New user connected');
 
-    socket.emit('newMessage', { // server发给自己
-        form: 'Admin',
-        text: 'Welcome to the chat app'
-    });
+    socket.emit('newMessage', generateMessage('Admin', 'Welcome to the chat app'));
 
-    socket.broadcast.emit('newMessage', { // server发给除去自己的所有人
-        from: 'Admin',
-        text: 'New user joined',
-        createdAt: new Date().getTime()
-    });
+    socket.broadcast.emit('newMessage', generateMessage('Admin', 'New user joined'));
 
     socket.on('createMessage', (message) => {
         console.log('createMessage', message);
-        io.emit('newMessage', { // server发给所有人
-            from: message.from,
-            text: message.text,
-            createdAt: new Date().getTime()
-        });
+        io.emit('newMessage', generateMessage(message.from, message.text));
     });
 
     socket.on('disconnect', () => {
