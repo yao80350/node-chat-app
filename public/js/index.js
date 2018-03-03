e05946d5a462cd025eda919d835a81bd61c248b9
Yao <151736726@qq.com>
2018-03-03 18:32:16 +0800

    Add geolocation support via geolocation api

--- public/js/index.js
+++ public/js/index.js
@@ -17,6 +17,17 @@ socket.on('newMessage', function(message) {
     jQuery('#messages').append(li);
 });
 
+socket.on('newLocationMessage', function(message) {
+    // 后端数据与li a 分开(防止jQuery送的string被篡改)
+    var li = jQuery('<li></li>');
+    var a = jQuery('<a target="_blank">This is my current location</a>');
+
+    li.text(`${message.from}: `);
+    a.attr('href', message.url);
+    li.append(a);
+    jQuery('#messages').append(li);
+});
+
 jQuery('#message-form').on('submit', function(e) {
     e.preventDefault();
 
@@ -25,5 +36,26 @@ jQuery('#message-form').on('submit', function(e) {
         text: jQuery('[name=message]').val()
     }, function() {
 
+    });
+});
+
+var locationButton = jQuery('#send-location');
+locationButton.on("click", function(e) {
+    // PC geolocation 需要用到google定位服务，所有需要翻墙。手机有google定位的话可以用。
+    // position是翻墙给的position
+    // 国内可以用 https://api.map.baidu.com/location/ip?ip=&ak=54HTqRT5AzWhub69FfOhGUrVaPiXX8qL&coor=bd09ll
+    if (!navigator.geolocation) {
+        return alert("Geolocation not supported by your browser.");
+    }
+
+    navigator.geolocation.getCurrentPosition(function(position) {
+        console.log(position);
+        socket.emit('createLocationMessage', {
+            latitude: position.coords.latitude,
+            longitude: position.coords.longitude
+        });
+    }, function() { // err 比如拿position需要用户同意,拒绝会失败
+        //上线后的链接需要是https才能用geolocation
+        alert('Unable to fetch location.');
     });
 });
--- server/utils/message.test.js
+++ server/utils/message.test.js
@@ -1,6 +1,19 @@
 const expect = require('expect');
 
-const {generateMessage} = require('./message');
+const {generateMessage, generateLocationMessage} = require('./message');
+
+describe('generateLocationMessage', () => {
+    it('should generate correct location object', () => {
+        let from = 'Deb';
+        let latitude = 15;
+        let longitude = 19;
+        let url = 'https://www.google.com/maps?q=15,19';
+        let message = generateLocationMessage(from, latitude, longitude);
+
+        expect(message.createdAt).toBeA('number');
+        expect(message).toInclude({from, url});
+    });
+});
 
 describe('generateMessage', () => {
     it('should generate correct message object', () => {
